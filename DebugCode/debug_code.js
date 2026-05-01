8a079081c9aed2a9ec1cb77bcbda171a73ba7fa8
Adam Elhabian <elhabianadam16@gmail.com>
2026-05-01 16:26:40 +0300

    do Arthimatic Operations simultaneously

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -3,19 +3,38 @@ function performOperation() {
   let num2 = parseInt(document.getElementById("input2").value);
 
   if (!isNaN(num1) && !isNaN(num2)) {
-    let result = multiply(num1, num2);
+    try {
+      if (num2 == 0) throw new Error("Cannot Divide by Zero");
 
-    displayResult(result);
+      let result1 = multiply(num1, num2);
+      let result2 = add(num1, num2);
+      let result3 = division(num1, num2);
+
+      displayResult(result1, result2, result3);
+    } catch (error) {
+      console.log(error.message);
+    }
   } else {
-    displayResult("please enter valid numbers");
+    displayResult("Please enter valid numbers");
   }
 
   function multiply(a, b) {
-    debugger;
     return a * b;
   }
-  function displayResult(result) {
+
+  function add(a, b) {
+    return a + b;
+  }
+
+  function division(a, b) {
+    return a / b;
+  }
+
+  function displayResult(result1, result2, result3) {
     const resultElement = document.getElementById("result");
-    resultElement.innerText = `The result is : ${result}`;
+
+    resultElement.innerHTML = `The result of Multiplication is : ${result1}<br>
+       The result of Addition is : ${result2}<br>
+       The result of Division is : ${result3}`;
   }
 }
